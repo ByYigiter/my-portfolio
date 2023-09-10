c9a66c38d7238f35e20937d66aab72765eee4d09
Yigit Er <mr.yigiter222@gmail.com>
2023-09-10 22:01:53 +0300

    Footer update

--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -2,13 +2,12 @@ import { useState, useEffect } from "react";
 import { Link } from "react-scroll";
 
 function Navbar() {
-
 	const [navActive, setNavActive] = useState(false);
 
 	const toggleNav = () => {
 		setNavActive(!navActive);
 	};
-	
+
 	const closeMenu = () => {
 		setNavActive(false);
 	};
